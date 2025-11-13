1e85536faf4ac4ea93a8a9ef9c405b51e7a8b83b
Vivek <dubeyvivek48@gmail.com>
2025-11-13 16:13:43 +0530

    connect to db create 2 apis of user to get user and create user

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
-console.log('Hello, World!|||');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const { connectDB } = require('./config/database');
+const UserModel = require('./models/user');
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const userObj = { firstName, lastName, email, password };
+
+  const user = new UserModel(userObj);
+
+  await user.save();
+  res.send('User signed up successfully');
+});
+
+app.get('/users', async (req, res) => {
+  const users = await UserModel.find();
+  res.json(users);
+});
+
+connectDB()
+  .then(() => {
+    console.log('Connected to the database successfully');
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect ', err);
+  });
